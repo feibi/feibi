a6713220ff123532e1e2a439c2353f16de59b15f
zylee <zylee005@163.com>
2016-07-11 23:47:39 +0800

    svg-sprite

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     clean = require('gulp-clean'),
-    svgSprite = require('gulp-svg-sprite');
+    svgSprite = require('gulp-svg-sprite'),
+    plumber = require('gulp-plumber'),
+    fileInclude = require('gulp-file-include');
 
 var config = {
     shape: {
@@ -13,35 +15,57 @@ var config = {
         }
     },
     mode: {
-        view: { // Activate the «view» mode
-            bust: true,
-            render: {
-                scss: true, // Activate Sass output (with default options)
-                css: true
-            },
-            sprite: "../fonts/sprite.name.svg",
-            dimensions: '-name',
-            dest: 'sass',
-            example: false
-        },
-        symbol: false // Activate the «symbol» mode
-
-    }
+        // view: { // Activate the «view» mode
+        //     bust: true,
+        //     render: {
+        //         scss: true, // Activate Sass output (with default options)
+        //         css: true
+        //     },
+        //     sprite: "../fonts/sprite.name.svg",
+        //     dimensions: '-name',
+        //     dest: 'sass',
+        //     example: false
+        // },
+        symbol: {
+            dest: './',
+            dimensions: '-icon',
+            render:{
+              scss:{
+                dest:'sass/sprite.scss'
+              }
+            }
+        }
+    } // Activate the «symbol» mode
 };
 gulp.task('svg', function() {
     gulp.src('app/images/svg/*.svg')
+        .pipe(plumber())
         .pipe(svgSprite(config))
+        .pipe(gulp.dest('./app'));
+});
+gulp.task('symbol', ['svg'], function() {
+    gulp.src('app/svg/*.svg')
+        .pipe(plumber())
+        .pipe(gulp.dest('./dist/svg'))
+});
+gulp.task('include', ['symbol'], function() {
+    gulp.src(['app/index.html'])
+        .pipe(plumber())
+        .pipe(fileInclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
         .pipe(gulp.dest('./dist'));
 });
-
 gulp.task('clean', function() {
     gulp.src('./dist', {
             read: false
         })
+        .pipe(plumber())
         .pipe(clean({
             force: true
         }))
 });
 
 
-gulp.task('default', ['clean', 'svg']);
+gulp.task('default', ['clean', 'include']);
